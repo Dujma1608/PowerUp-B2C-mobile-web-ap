be621d312056b393b241bfb6408dc3c0e3ae0bf0
Pavle <radomanp@gmail.com>
2023-01-03 14:35:23 +0100

    Onboarding pages

---
+++ src/app/onboarding/onboarding.page.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-onboarding1',
+  templateUrl: './onboarding.page.html',
+  styleUrls: ['./onboarding.page.scss',],
+})
+export class OnboardingPage implements OnInit {
+  currentPage = 3;
+
+  onboardingTitles = [
+    "Charger",
+    "Connect car",
+    "Scan QR code",
+    "Start charging"
+  ]
+
+  onboardingSubtitles = [
+    "Park close to charger",
+    "Plug in charging cable into your car",
+    "Use your phone to scan QR code displayed on charger to start charging",
+    "You can monitor charging status from \n anywhere within mobile app"
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  goToNextSlide() {
+    this.currentPage++
+
+    if(this.currentPage == this.onboardingTitles.length+1) {
+      this.currentPage =1
+    }
+  }
+
+}
